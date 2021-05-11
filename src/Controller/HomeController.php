9f7bc821e49c84973075f1cb132670e8efecc957
Soupramanien BOUVANESVARY <soupramanien@baobab-ingenierie.fr>
2021-05-11 14:33:39 +0200

    optionnal parametre added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,19 +5,45 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-/**
- * @Route(path="/home", name="home_route")
- */
+
 class HomeController extends AbstractController
 {
     /**
-     * @Route(path="/", name="home_index_route2")
-     * @Route(path="/index", name="home_index_route")
+     * @Route(path="/home/{nom?}", name="home_index0_route")
     */
-    public function index(): Response
+    public function index0(?string $nom): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => $nom,
+        ]);
+    }
+    /**
+     * @Route(path="/home/{nom}", name="home_index_route", priority=2)
+    */
+    public function index(string $nom): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => $nom . " route2" ,
+        ]);
+    }
+
+    /**
+     * @Route(path="/home/age/{age}", name="home_index2_route", requirements={"age"="\d{2,3}"})
+    */
+    public function index2(int $age): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => $age,
+        ]);
+    }
+
+    /**
+     * @Route(path="/home/age2/{age<\d{2,3}>}", name="home_index3_route")
+    */
+    public function index3(int $age): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => $age,
         ]);
     }
 }
