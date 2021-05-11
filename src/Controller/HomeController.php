aad3b1a487212698adf68ca01358a85e57312ceb
Soupramanien BOUVANESVARY <soupramanien@baobab-ingenierie.fr>
2021-05-11 13:52:48 +0200

    multi-route added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,10 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route(path="/home", name="home_route")
+ */
 class HomeController extends AbstractController
 {
-    
+    /**
+     * @Route(path="/", name="home_index_route2")
+     * @Route(path="/index", name="home_index_route")
+    */
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
